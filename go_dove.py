ddc3207ca22009f0c558050faa163bd6e3a1bcfc
thotran2015 <thotran2015@gmail.com>
2017-01-12 20:00:29 -0500

    marshall_scientific is tentatively done

--- go_dove.py
+++ go_dove.py
@@ -4,11 +4,31 @@ Created on Wed Jan 11 00:49:07 2017
 
 @author: thotran
 """
+import urllib2
+from bs4 import BeautifulSoup
+
 main_url='http://www.go-dove.com/en/auction/search?cmd=results&fromsearch=true&words='
 
 def search_url(search_word):
+    if len(search_word)==0:
+        return 'Please enter a keyword for searching'
     if ' ' in search_word:
         search=search_word.replace(' ', '+')
     else:
         search=search_word
-    return main_url+search+'&Submit='
+    return main_url+search
+    
+    
+def equipment_properties(search_word):
+    web=search_url(search_word)
+    page=urllib2.urlopen(web).read()
+    soup=BeautifulSoup(page)
+    #product_grid=soup.find('table',class_='results-table table table-striped')
+    products=soup.find('tbody')
+    print products.find('tr')
+    #for row in products.find('tr'):
+        #return row
+    #return products
+    
+    
+print equipment_properties('centrifuge')
--- util.py
+++ util.py
@@ -1,8 +1,40 @@
 import re
 import string
-
+import numpy
+from Result import *
 #Helper function to get price
 def get_price(price):
+    #price input is string
   allow = string.digits
   number = re.sub('[^%s]' %allow,'',price)
   return number
+
+
+def min_price(results):
+    #results is list of result objects
+    prices=[]
+    for equip in results:
+        if equip.price!=None and equip.price!='': 
+            prices.append(equip.price)
+        
+    return min(prices)
+    
+
+def max_price(results):
+    prices=[]
+    for equip in results:
+        if equip.price!=None and equip.price!='': 
+            prices.append(equip.price)
+        
+    return max(prices)
+    
+def median_price(results):
+    prices=[]
+    for equip in results:
+        if equip.price!=None and equip.price!='': 
+            prices.append(equip.price)
+    return numpy.median(numpy.array(prices))
+    
+    
+        
+        
