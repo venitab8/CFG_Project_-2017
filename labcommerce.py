f90a9854dda688df9a1f024f6e182bc20675b2b6
Venita Boodhoo <venitab@mit.edu>
2017-01-13 19:56:34 -0500

    VB: commented code and still on labcommerce

--- labcommerce.py
+++ labcommerce.py
@@ -1,44 +1,58 @@
-#LabCommerce
-#Status: In Progress
+"""
+@author Venita Boodhoo
+Website: LabCommerce
+Status: In Progress
+Note: LabCommerce is also a reseller of previously-owned, 
+       used and surplus/unused laboratory equipment
+"""
 
-import re
-import string
 import urllib2
 from bs4 import BeautifulSoup
-testpage = "http://www.labcommerce.com/searchresults.php?txtsearch=air+pump&image.x=0&image.y=0"
-page = urllib2.urlopen(testpage)
-soup = BeautifulSoup(page,"html.parser")
+from util import *
+from Result import Result
 
-table = soup.find('tbody', class_='ResultsNewTable')
-#For used condition add the below comment to url string:
-#&condition=Used,Refurbished,For%20Parts/Not%20Working,New%20or%20Used&adtype=998
+page = "http://www.labcommerce.com/searchresults.php?txtsearch="
 
-##Description
+def create_url(item):
+	specific_url=page
+	search_words=item.split()
+	for i in range(len(search_words)):
+		if i!=0:
+			specific_url= specific_url + "+"+ search_words[i]
+		else:
+			specific_url= specific_url + search_words[i]
+	specific_url = specific_url + "&image.x=0&image.y=0"
+	return specific_url
 
-#Title
-A = []
-#Link
-B = []
-#Price
-C = []
+def get_results(item):                
+        page = urllib2.urlopen(create_url(item))
+        soup = BeautifulSoup(page,"html.parser" )
 
-#Description
-D = []
-#Seller Info
-E = []
-#Image
-F = []
+        table = soup.find_all('div',id="results")
+        #print table
+        print "-----------------"
+        for row in table:
+                print row.a.title
+                new_result = Result(row.find('a').title)
+                print new_result.title
+                new_result.url = create_url(item)+row.find('a').get('href')
+        
+        results=[]
+        print "----"
+        for row in table:
+                #print row
+                new_result = Result(row.find('a').get('title'))
+                #print new_result.title
+                new_soup = BeautifulSoup(urllib2.urlopen(new_result.url),"html.parser")
+                new_result.price = new_soup.find('a')
+                number = get_price(new_result.title)
+                new_result.condition = "used"
+                new_result.image_src = "https://photos.labx.com/labx/"+number+"/"+number+"-0.jpg"
+                results.append(new_result)
+                results.append("\n")
+        return results
 
-for row in table.find_all('tr'):
-  A.append(row.find('a').get('title'))
-  B.append(row.find('a').get('href'))
-  C.append(row.find_all('td')[4].contents[0])
-  new_soup = BeautifulSoup(urllib2.urlopen(B[-1]),"html.parser")
-  #D.append(new_soup.find('span'),class_='AdDetailsTitle')
-  E.append(new_soup.find('div', class_='sellerInformation'))
-  allow = string.digits
-  number = re.sub('[^%s]' %allow,'',A[-1])
-  F.append("https://photos.labx.com/labx/"+number+"/"+number+"-0.jpg")
+def main():
+    print get_results("pump")
 
-#print D
-
+main()
--- util.py
+++ util.py
@@ -2,9 +2,14 @@ import re
 import string
 import numpy
 from Result import *
-#Helper function to get price
+
+'''
+Takes in a string containing the price of the equipment and results the price only (decimal in a string)
+@param price a String of text containing the price of the equipment
+@return number a String containing only numbers and a decimal pt representing the price of the equipment
+'''
 def get_price(price):
-    #price input is string
+  #price input is string
   allow = string.digits
   number = re.sub('[^%s]' %allow,'',price)
   return number
