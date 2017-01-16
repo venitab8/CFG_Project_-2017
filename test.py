02b25a9f8fa2aeaa70d8f8d4ace9e9a132268fc5
Venita Boodhoo <venitab@mit.edu>
2017-01-16 15:35:46 -0500

    VB: Editing main.py

--- test.py
+++ test.py
@@ -2,19 +2,11 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
-    return render_template('main_equipment_page.html')
+@app.route("/")
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('main_equipment_page.html',name=name)
 
-@app.route('/search/<condition>')
-def enter_values(condition=None):
-    return "search page"
-
-
-@app.route('/results/<condition>')
-def results(condition=None):
-	return "result page"
-    
 def finish(self):
          if not self.wfile.closed:
             self.wfile.flush()
@@ -25,6 +17,6 @@ def finish(self):
                 # the local error ECONNABORTED.
                 pass
          self.wfile.close()
-         self.rfile.close()
+         self.rfile.close()    
 if __name__== "__main__":
     app.run()
