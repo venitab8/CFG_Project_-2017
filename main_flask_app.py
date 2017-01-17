3525d29b647aec874f21d410fd6d7d85224a7a8b
Venita Boodhoo <venitab@mit.edu>
2017-01-16 20:32:57 -0500

    Editing search box

--- main_flask_app.py
+++ main_flask_app.py
@@ -1,15 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 app = Flask(__name__)
+
+
+search_words = []
 
 @app.route('/')
 def main_page():
     return render_template('main_equipment_page.html')
 
 @app.route('/search/<condition>')
-def enter_values(condition=None):
-    return "search page"
+def display_search_page(condition=None):
+    return render_template('search_page.html')
 
+'''Currently request.form is empty :('''
+@app.route('/search/',methods=['GET','POST'])
+def enter_values():
+    #search_words = request.args('search')
+    #search_words = request.form['text']
+    return search_words
 
 @app.route('/results/<condition>')
 def results(condition=None):
@@ -26,5 +35,6 @@ def finish(self):
                 pass
          self.wfile.close()
          self.rfile.close()
+         
 if __name__== "__main__":
     app.run()
