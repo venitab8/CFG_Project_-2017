a24a32af55d42e8dd73a29fae9b911a36b94fda4
thotran2015 <thotran2015@gmail.com>
2017-01-18 22:19:02 -0500

    u

--- main_flask_app.py
+++ main_flask_app.py
@@ -22,7 +22,7 @@ def display_search_page(condition=None):
 @app.route('/results/<condition>/')
 def results(search_words=None):
     search_words = request.args.get('search')
-    return render_template('result_page', search_words=search_words)
+    return render_template('result_page.html', search_words=search_words)
 
 
     
