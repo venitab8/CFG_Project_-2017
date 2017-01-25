cc343e798613ac82f53cdd6d306adbbe3bf90968
Venita Boodhoo <venitab@mit.edu>
2017-01-24 20:28:18 -0500

    Change port

--- main_flask_app.py
+++ main_flask_app.py
@@ -42,6 +42,6 @@ def finish(self):
          self.rfile.close()
         
 if __name__== "__main__":
-    #port = int(environ.get('PORT', 5000))
-    #app.run(host='127.0.0.1', port=port)
-    app.run()
+    port = int(environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
+    #app.run()
