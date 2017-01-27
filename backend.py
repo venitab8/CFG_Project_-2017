43f6e803b04755f5ef9b68a341b0342c114c2f69
Abigail Katcoff <abigailkatcoff@dhcp-18-111-45-88.dyn.mit.edu>
2017-01-26 21:56:29 -0500

    fixing function names backend error

--- backend.py
+++ backend.py
@@ -24,7 +24,8 @@ dotmed.extract_results, sibgene.extract_results, labx.extract_results] #sibgene 
 
 WEBSITE_NAMES={ebay.extract_results : "ebay" , equipnet.extract_results : "equipnet" , google.extract_results : "google" , used_line.extract_results : "used line", \
 eurekaspot.extract_results : "eurekaspot", labcommerce.extract_results :"labcommerce", newlifescientific.extract_results :"newlifescientific", biosurplus.extract_results: "biosurplus" , sci_bay.extract_results : "sci_bay", \
-dotmed.extract_results : "dotmed" , sibgene.extract_results: "sibgene" , labx.extract_results : "labx"}
+dotmed.extract_results : "dotmed" , sibgene.extract_results: "sibgene" , labx.extract_results : "labx", medwow.extract_results: "medwow", marshallscientific.extract_results: \
+"marshallscientific"}
 
 MATCH_RATIO=.8
 
