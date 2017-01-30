bf675bd4011d9c5956e3fa15a97e571b29057a0e
thotran2015 <thotran2015@gmail.com>
2017-01-30 17:18:58 -0500

    updating

--- main_flask_app.py
+++ main_flask_app.py
@@ -5,8 +5,13 @@ from flask import render_template, request, redirect
 from os import environ
 import flask_excel as excel
 import json
+from flask import get_template_attribute
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///results.sqlite3'
+result_groups={}
+global result_groups
 
 @app.route('/')
 def main_page():
@@ -23,11 +28,12 @@ def run_search(condition=None):
     search_words = request.args.get('search')
     func_group = request.args.get('func_group')
     is_keyword_matched, message, results= backend.do_search(search_words,condition, func_group)
+    result_groups[search_words]=results
     if len(results)>3 or func_group=='2':
         median = util.price_prettify(util.median_price(results))
         for item in results:
             item.price = util.price_prettify(util.str_to_float(item.price))
-        return render_template('result_page.html', search_words=search_words,result=results, median=median,message=message, condition=condition)
+        return render_template('result_page.html', search_words=search_words,result=result_groups[search_words], median=median,message=message, condition=condition)
     else:
         return redirect('/results/'+ condition + "/?search=" + search_words + "&func_group=" + '2')
 
@@ -37,8 +43,19 @@ def download_file(search_words, condition=None):
     exported_list=[['Title','Price', 'Image', 'URL']]
     for r in result:
         exported_list.append([r.title, r.price, r.image_src, r.url])
-    print exported_list
     return excel.make_response_from_array(exported_list, "xls")
+    
+@app.route('/sort/<search_words>/', methods=['GET'])
+def sort_by(search_words, condition=None):
+    #is_keyword_matched, message, results= backend.do_search(search_words,condition)
+    #result=get_template_attribute('/results/<condition>/', 'result')
+    #print result
+    sorted_results= sorted(result_groups[search_words],reverse=False)
+    median = util.price_prettify(util.median_price(sorted_results))
+    return render_template('result_sorted.html', search_words=search_words, sorted_result=sorted_results,median=median,condition=condition)
+  
+
+
 
 def finish(self):
          if not self.wfile.closed:
@@ -53,6 +70,7 @@ def finish(self):
          self.rfile.close()
         
 if __name__== "__main__":
+    
     port = int(environ.get('PORT', 5000))
     app.run(host='127.0.0.1', port=port)
     #app.run()
