fe260a9d5d5a16f2239493a120c67b98f3da8cca
Venita Boodhoo <venitab08@gmail.com>
2020-03-28 21:49:03 -0400

    Labcommerce complete, removed duplicate results

--- labcommerce.py
+++ labcommerce.py
@@ -11,8 +11,8 @@ from util import *
 from Result import Result
 
 
-MAIN_URL = 'http://www.labcommerce.com'
-SEARCH_URL = "http://www.labcommerce.com/searchresults.php?txtsearch="
+MAIN_URL = 'https://www.labcommerce.com'
+SEARCH_URL = "https://www.labcommerce.com/searchresults.php?txtsearch="
 DELIMITER = "+"
 
 def extract_results(item,condition=None):
@@ -23,21 +23,18 @@ def extract_results(item,condition=None):
                 table = soup.find_all('div',class_="search_result")
                 
                 for row in table:
-                        #.encode('utf-8') is used because to convert text to str since
-                        #not all of the characters are recognized
-                        new_result = Result(row.a.text.encode('utf-8').strip())
+                        match = False
+                        new_result = Result(row.a.text)
                         url = re.sub('/catid/','.php?catid=',row.find('a').get('href'))
                         #Omit last slash
                         specific_url = re.sub('/prodid/','&prodid=',url)[:-1]
-                        new_result.url = MAIN_URL+ specific_url.encode('utf-8').strip()
-                        new_soup = BeautifulSoup(urllib2.urlopen(new_result.url),"html.parser")
+                        new_result.url = MAIN_URL + specific_url
+                        new_soup = BeautifulSoup(urllib.request.urlopen(new_result.url),"html.parser")
                         #Omit surrounding text, get decimal only
-                        new_result.price = get_price(str(new_soup.find('td',class_='price').find_all(text=True)[0])\
-                                           .encode('utf-8').strip())
+                        new_result.price = get_price(str(new_soup.find('td',class_='price').find_all(text=True)[0]))
                         #Omit 1st char (a period)
                         new_result.image_src = MAIN_URL+new_soup.find('td',align='center')\
-                                               .find('img').get('src')[1:].encode('utf-8').strip()
-                        
+                                               .find('img').get('src')[1:]
                         bad_condition_types = ['bad','poor','not working','broken','not functional']
                         condition_type_text = new_soup.find(text='Condition:')
                         #Matches on condition and omits bad conditions
@@ -45,13 +42,18 @@ def extract_results(item,condition=None):
                                 condition_type = condition_type_text.find_next(text=True)
                                 for word in bad_condition_types:
                                         if word not in condition_type and is_valid_price(new_result.price):
-                                                results.append(new_result)
+                                                match = True
+                                if match:
+                                        results.append(new_result)
+                                if len(results) == 9:
+                                        break
                         elif is_valid_price(new_result.price):
                                 results.append(new_result)
-        
+                        if len(results) == 9:
+                                break     
         return results
 
 def main():
-    print (extract_results("pump"))
+    print (extract_results("Vacuum Stainless Flex Hose"))
 
 if __name__ == "__main__": main()
