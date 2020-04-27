046be5326958ae74e3e914e35c1a28c5fa1929c1
Venita Boodhoo <venitab08@gmail.com>
2020-04-26 21:01:15 -0400

    Finished ebay

--- ebay.py
+++ ebay.py
@@ -1,5 +1,6 @@
 '''
 Created by Abigail Katcoff (complete)
+Modified by Venita Boodhoo (04/2020)
 New and Used Equipment
 '''
 import util
@@ -11,32 +12,32 @@ MAIN_URL='http://www.ebay.com/sch/i.html?_nkw='
 DELIMITER= '+'
 
 USED= '&LH_ItemCondition=4'
-NEW= '&LH_ItemCondition=1'
+NEW= '&LH_ItemCondition=3'
 
 def extract_results(search_term, condition=None):
 	url=''
 	if condition=='new':
-		url = util.create_url(MAIN_URL, search_term, DELIMITER) + '&LH_BIN=1' + NEW
+		url = util.create_url(MAIN_URL, search_term, DELIMITER) + '&rt=nc' + NEW
 	else:
-		url = util.create_url(MAIN_URL, search_term, DELIMITER) + '&LH_BIN=1' + USED
+		url = util.create_url(MAIN_URL, search_term, DELIMITER) + '&rt=nc' + USED
 	page=urllib.request.urlopen(url)
 	soup = BeautifulSoup(page,"html.parser")
-	table=soup.find('div', id='ResultSetItems')
+	table=soup.find('div',class_='srp-river-results clearfix')
 	try:
-		rows=table.findAll('li', class_='sresult lvresult clearfix li')
+		rows=table.findAll('div', class_='s-item__wrapper clearfix')
 	except:
 		return []
 	results=[]
 	for row in rows: 
-		new_result=Result(row.find('h3', class_="lvtitle").find(text=True))
-		new_result.url=row.find('h3', class_="lvtitle").find('a').get('href')
-		new_result.image_src=row.find('img', class_='img').get('src')
-		new_result.price=util.get_price(row.find('li', class_="lvprice prc").find('span').find(text=True))
-		if util.is_valid_price(new_result.price):
+		new_result=Result(row.find('img', class_='s-item__image-img').get('alt'))
+		new_result.set_url(row.find('a').get('href'))
+		new_result.set_image_src(row.find('img', class_='s-item__image-img').get('src'))
+		new_result.set_price(util.get_price(row.find('span', class_="s-item__price").text))
+		if util.is_valid_price(new_result.get_price()):
 			results.append(new_result)
 	return results
 
 def main():
-    print (extract_results("vacuum bump"))
+	print (extract_results("vacuum pump","new"))
 
 if __name__ == "__main__": main()
