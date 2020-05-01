f1d8eaaa00e3cb7ba139468dd91ec9141c742faf
Venita Boodhoo <venitab08@gmail.com>
2020-05-01 13:56:07 -0400

    Updated class methods for marshallscientific

--- marshallscientific.py
+++ marshallscientific.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan  9 18:06:56 2017
-
+Modified by Venita Boodhoo (05/2020)
 @author: thotran
 Marshall Scientific sells used equipment only. 
 """
@@ -29,15 +29,15 @@ def extract_results(search_word, condition=None):
     equips=[]
     
     for equip in total_equips:
-        title=equip.find('a', class_='v-product__title productnamecolor colors_productname').find(text=True).strip()
+        title=equip.find('a', class_='v-product__title productnamecolor colors_productname').text.strip()
         equipment=Result(title)
-        equipment.url=equip.find('a',class_='v-product__img').get('href')
-        equipment.image_src='http:'+equip.find('img').get('src')
-        price_text=equip.find('div', class_='product_productprice').find_all(text=True)
-        equipment.price=util.get_price(''.join(price_text))
+        equipment.set_url(equip.find('a',class_='v-product__img').get('href'))
+        equipment.set_image_src('https:'+equip.find('img').get('src'))
+        price_text=equip.find('div', class_='product_productprice').text
+        equipment.set_price(util.get_price(''.join(price_text)))
         if util.is_valid_price(equipment.price):
             equips.append(equipment)
-        if len(equips)>=10:
+        if len(equips)==10:
             return equips
     return equips
 
